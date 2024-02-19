aebb3aee915b917f5345c4c2a674372e82556f5e
Domenico Scognamiglio <159646189+MimmoScogna@users.noreply.github.com>
2024-02-20 00:03:51 +0100

    Update 2_👕_Team_Stats.py

--- pages/2_👕_Team_Stats.py
+++ pages/2_👕_Team_Stats.py
@@ -32,6 +32,13 @@ button[title="View fullscreen"]{
     visibility: hidden;}
 </style>
 '''
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown(hide_img_fs, unsafe_allow_html=True)
 
